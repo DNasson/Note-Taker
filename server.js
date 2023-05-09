8fc6e32577d17c6f5432df3f7fac4b46f8967600
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-05-08 22:30:42 -0600

    "add json.package data, add info server.js"

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-// GET /notes = notes.html
-// GET * = index.html
-
-// GET /api/notes = db.json
-
-// POST /api/notes = db.json
-
 const express = require('express');
 const path = require('path');
+const index = require('./public/index.html');
+const notes = require('./public/notes.html');
+const db = require('./db/db.json');
 
 const app = express();
-const port = 3001;
+const PORT = 3001;
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => res.json(notes));
+app.get('*', (req, res) => res.json(index));
+
+app.get('/api/notes', (req, res) => res.json(db.json));
+app.post('/api/notes', (req, res) => res.json(db.json));
+
+
 
 app.listen(PORT, () => 
     console.log(`Example app listening at http://localhost:${PORT}`))
