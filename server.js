eb1af8c173446bc692d4318670036279f193a4e9
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-05-08 22:00:52 -0600

    "link html folders, start server.js"

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-GET /notes = notes.html
-GET * = index.html
+// GET /notes = notes.html
+// GET * = index.html
 
-GET /api/notes = db.json
+// GET /api/notes = db.json
 
-POST /api/notes = db.json
+// POST /api/notes = db.json
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => 
+    console.log(`Example app listening at http://localhost:${PORT}`))
