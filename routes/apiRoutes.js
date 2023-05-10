478b51faca9c9164622e3007b6ad84a202717f53
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-05-10 13:00:51 -0600

    "fix html's to work, make routes"

---
+++ routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const path = require('path');   
+
+router.get('/api/notes', (req, res) => 
+    console.info(`${req.method} request received for notes`));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+
+router.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (title && text) { 
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+});
+
