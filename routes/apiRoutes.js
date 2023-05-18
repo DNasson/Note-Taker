765464bff7085156464c684d677b39841bf0a116
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-05-18 12:38:01 -0600

    "add db info, fix api routes bug"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,31 @@
 const express = require('express');
 const router = require('express').Router();
+const fs = require('fs');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils.js');
 const DB = require('../db/db.json');
 
-router.get("/api/notes", async function (req, res) {
-    const notes = await DB.readNotes();
-    return res.json(notes);
+router.get("/api/notes", (req, res) => {
+   console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
-  router.post("/api/notes", async function (req, res) {
-    const currentNotes = await DB.readNotes();
-    let newNote = {
-      id: uuid(),
-      title: req.body.title,
-      text: req.body.text,
-    };
-  
-    await DB.addNote([...currentNotes, newNote]);
-  
-    return res.send(newNote);
+  router.post("/api/notes", (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {    
+        res.error('Error in adding note');
+    }
+
   });
 
   router.delete("/api/notes/:id", async function (req, res) {
