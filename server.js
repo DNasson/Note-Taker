22fe815667108604afc7c9b48ab54ec8bd237085
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-05-19 14:17:27 -0600

    "fix port"

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./routes/apiRoutes');
 
 // create app and set port
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT||3001;
 
 // data parsing middleware
 app.use(express.json());
